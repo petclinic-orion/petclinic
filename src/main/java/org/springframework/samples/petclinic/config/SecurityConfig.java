4efde6eebfd0740ffe81ad88efed335ef5bdbd0a
musyil <mustafa.yildirim@orioninc.com>
2024-08-19 13:19:30 +0300

    add jwt

---
+++ src/main/java/org/springframework/samples/petclinic/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package org.springframework.samples.petclinic.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+	private final JwtRequestFilter jwtRequestFilter;
+
+	@Autowired
+	public SecurityConfig(@Lazy JwtRequestFilter jwtRequestFilter) {
+		this.jwtRequestFilter = jwtRequestFilter;
+	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(HttpSecurity http, UserDetailsService userDetailsService) throws Exception {
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+			.userDetailsService(userDetailsService)
+			.and()
+			.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("demoUser")
+			.password("{noop}password")
+			.roles("USER")
+			.build();
+
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(authorize -> authorize
+				.requestMatchers("/authenticate").permitAll()  // Permit access to the authentication endpoint
+				.anyRequest().authenticated()  // All other requests require authentication
+			)
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));  // Use stateless session management
+
+		// Add JWT filter before the UsernamePasswordAuthenticationFilter
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+}
+
